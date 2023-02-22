91068abadf8437e5c0c9009a136a535515fb593b
Kenneth Laskoski <kennethlaskoski@gmail.com>
2023-02-22 14:52:23 -0300

    Initial Commit

---
+++ Algorithm/AlgorithmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmApp.swift
+//  Algorithm
+//
+//  Created by Kenneth Laskoski on 2023-02-22.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
