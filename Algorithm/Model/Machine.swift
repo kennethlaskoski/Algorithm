3f11934a5f5f6268afa55b523d0dce270d290109
Kenneth Laskoski <kennethlaskoski@gmail.com>
2024-05-31 15:13:07 -0300

    Created group to separate neander files

--- Algorithm/Model/Machine.swift
+++ Algorithm/Model/Machine.swift
@@ -9,31 +9,3 @@ protocol Machine: ObservableObject {
   func step()
   func reset()
 }
-
-protocol Register: Identifiable {
-  associatedtype Value: FixedWidthInteger
-
-  var name: String { get }
-  var value: Value { get set }
-}
-
-extension Register {
-  var id: String { name }
-}
-
-protocol Flag: Identifiable {
-  var name: String { get }
-  var isOn: Bool { get set }
-}
-
-extension Flag {
-  var id: String { name }
-}
-
-protocol MemoryType: RandomAccessCollection {
-  var rAddr: Index { get set }
-  var rData: Element { get set }
-
-  mutating func read()
-  mutating func write()
-}
